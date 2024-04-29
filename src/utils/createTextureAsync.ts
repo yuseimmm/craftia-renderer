a6bbfc77171a2dda3d2dbabebc971a393a57ebe2
yuseimmm <yusei.mmm@outlook.jp>
2024-04-29 10:40:28 +0900

    add a useful function for creating textures

---
+++ src/utils/createTextureAsync.ts
@@ -0,0 +1,9 @@
+import { Texture } from '../core'
+
+export const createTextureAsync = async (src: string) => {
+    const img = new Image()
+    img.src = src
+    await img.decode()
+
+    return new Texture().setPixcels(img, img.width, img.height)
+}
