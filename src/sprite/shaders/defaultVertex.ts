2a8295848b1f5fdedb40c00c57d9924683f9b448
yuseimmm <yusei.mmm@outlook.jp>
2024-04-29 10:42:27 +0900

    update: make shader sources extensible

--- src/sprite/shaders/defaultVertex.ts
+++ src/sprite/shaders/defaultVertex.ts
@@ -1,6 +1,11 @@
 import { ShaderTemplate } from '../../types/Shaders'
 
-export const defaultVertex: ShaderTemplate = () => /*glsl*/ `#version 300 es
+export const defaultVertex: ShaderTemplate = ({
+    header,
+    func,
+    start,
+    main,
+} = {}) => /*glsl*/ `#version 300 es
 precision highp float;
 in vec2 a_position;
 
@@ -9,7 +14,12 @@ out vec2 v_textureCoord;
 
 uniform mat3 u_matrix;
 
+${header ?? ''}
+${func ?? ''}
+
 void main() {
+    ${start ?? ''}
     v_textureCoord = a_uv;
     gl_Position = vec4((u_matrix * vec3(a_position, 1)).xy, 0, 1);
+    ${main ?? ''}
 }`
