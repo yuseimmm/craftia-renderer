a1ae787285d98378a43c77c46bf2159ae22a8959
yuseimmm <yusei.mmm@outlook.jp>
2024-04-29 10:41:46 +0900

    refactor: inherit the `Container` class

--- src/layer/RasterLayer.ts
+++ src/layer/RasterLayer.ts
@@ -1,18 +1,72 @@
-import { TextureSprite, TextureSpriteOptions } from '../sprite/TextureSprite'
-import { Layer, LayerOptions } from './Layer'
+import { mat3 } from 'gl-matrix'
+import { Container, ContainerOptions } from '../contanier'
+import { FrameBuffer, Texture, Vec2, WebGLRenderer } from '../core'
+import { Effect } from '../effect/Effect'
+import { TextureSprite } from '../sprite'
+import { ILayer } from './ILayer'
+import { BLEND_MODES } from '../blend-modes'
 
-export type RasterLayerOptions = TextureSpriteOptions & LayerOptions
+export type RasterLayerOptions = Partial<ContainerOptions> & {
+    blendMode?: keyof typeof BLEND_MODES
+    translation?: Vec2
+    texture: Texture
+}
 
-export class RasterLayer extends Layer {
-    protected sprite: TextureSprite
+export class RasterLayer extends Container implements ILayer {
+    public blendMode: keyof typeof BLEND_MODES
+    public translation: Vec2
+    public source: Effect<TextureSprite>
+    public update: boolean
 
     constructor(options: RasterLayerOptions) {
         super(options)
-        this.sprite = new TextureSprite(options)
+
+        this.blendMode = options.blendMode ?? 'normal'
+        this.translation = options.translation ?? new Vec2(0, 0)
+        this.update = false
+
+        this.source = new Effect({
+            blendMode: this.blendMode,
+            opacity: this.getOpacity(),
+            sprite: new TextureSprite({
+                transform: this._createTransform(),
+                texture: options.texture,
+            }),
+        })
     }
 
-    public getRenderables() {
-        this.sprite.transform = this.getMatrix()
-        return this.visible ? [this.sprite] : []
+    public get children() {
+        this.source.opacity = this.getOpacity()
+        this.source.blendMode = this.blendMode
+
+        //TODO : Add layer effects.
+        return [this.source]
+    }
+
+    public updateTransform() {
+        this.source.sprite.transform = this._createTransform()
+    }
+
+    private _createTransform() {
+        const matrix = mat3.create()
+        mat3.translate(matrix, matrix, this.translation.toArray())
+        return matrix
+    }
+
+    public renderSource(renderer: WebGLRenderer, target: FrameBuffer) {
+        this.source.opacity = this.getOpacity()
+        this.source.blendMode = this.blendMode
+        this.source.sprite.render(renderer, target)
+    }
+
+    public clone() {
+        return new RasterLayer({
+            visible: this.visible,
+            blendMode: this.blendMode,
+            opacity: this.opacity,
+            parent: { ...this.parent },
+            translation: this.translation,
+            texture: this.source.sprite.texture,
+        })
     }
 }
