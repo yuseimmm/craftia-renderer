979c7e70da54bfcc20eda5384f5627464365047b
yuseimmm <yusei.mmm@outlook.jp>
2024-05-06 21:56:20 +0900

    refactor: add rendering options

--- src/sprite/ISprite.ts
+++ src/sprite/ISprite.ts
@@ -2,10 +2,17 @@ import { mat3 } from 'gl-matrix'
 import { FrameBuffer, Shader, WebGLRenderer } from '../core'
 
 export interface ISprite {
-    transform: mat3
-    shader: Shader
     width: number
     height: number
-    render(renderer: WebGLRenderer, target: FrameBuffer | null): unknown
+    render(
+        renderer: WebGLRenderer,
+        target: FrameBuffer | null,
+        options?: SpriteRenderOptions
+    ): unknown
     clone(): ISprite
+}
+
+export type SpriteRenderOptions = {
+    transform?: mat3
+    shader?: Shader
 }
