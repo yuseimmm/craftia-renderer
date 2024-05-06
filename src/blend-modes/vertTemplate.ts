83bc967cdb2aa3d1dfc069e17d233812d2965653
yuseimmm <yusei.mmm@outlook.jp>
2024-05-06 22:18:04 +0900

    chore: make shader sources extensible

--- src/blend-modes/vertTemplate.ts
+++ src/blend-modes/vertTemplate.ts
@@ -1,6 +1,11 @@
 import { ShaderTemplate } from '../types/Shaders'
 
-export const vertTemplate: ShaderTemplate = () => /*glsl*/ `#version 300 es
+export const vertTemplate: ShaderTemplate = ({
+    header,
+    func,
+    start,
+    main,
+} = {}) => /*glsl*/ `#version 300 es
 precision highp float;
 in vec2 a_position;
 
@@ -8,8 +13,11 @@ in vec2 a_uv;
 out vec2 v_textureCoord;
 
 uniform mat3 u_matrix;
-
+${header ?? ''}
+${func ?? ''}
 void main() {
+    ${start ?? ''}
     v_textureCoord = a_uv;
     gl_Position = vec4((u_matrix * vec3(a_position, 1)).xy, 0, 1);
+    ${main ?? ''}
 }`
