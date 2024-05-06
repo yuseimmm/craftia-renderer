c6e86536c64762c54d51a261ac17792bcfa65f9e
yuseimmm <yusei.mmm@outlook.jp>
2024-05-06 22:17:32 +0900

    fix: a bug that caused dark composite results when the lower layer is transparent

--- src/blend-modes/compositings/sourceOverComp.ts
+++ src/blend-modes/compositings/sourceOverComp.ts
@@ -5,9 +5,9 @@ export const sourceOverComp: BlendModeProps = {
     bit: {
         func: /*glsl*/ `
         vec4 compSourceOver(vec4 base, vec4 front) {
-            vec3 color = front.a * front.rgb + base.a * base.rgb * (1.0f - front.a);
+            vec3 color = base.a == 0.0f ? front.rgb : (front.a * front.rgb + base.a * base.rgb * (1.0f - front.a));
             float alpha = front.a + base.a * (1.0f - front.a);
-            return vec4(color.rgb, alpha);
+            return vec4(color, alpha);
         }`,
         main: /*glsl*/ `
         outColor = compSourceOver(base, front);
