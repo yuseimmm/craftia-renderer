c88e12f7014ead65cf6a1e1704acbc65beba579b
yuseimmm <yusei.mmm@outlook.jp>
2024-07-13 11:47:20 +0900

    remove `ILayer` interface

--- src/layer/ILayer.ts
+++
@@ -1,9 +0,0 @@
-import { BLEND_MODES } from '../blend-modes'
-import { Container } from '../contanier'
-import { Vec2 } from '../units'
-
-export interface ILayer<T> extends Container {
-    translation: Vec2
-    blendMode: keyof typeof BLEND_MODES
-    clone(options: T): ILayer<T>
-}
