380dd6b6db95067ad6e88dcb4a631231144603e0
yuseimmm <yusei.mmm@outlook.jp>
2024-07-13 11:57:23 +0900

    update the method of setting values for uniform variables

--- src/uniforms/UniformGroup.ts
+++ src/uniforms/UniformGroup.ts
@@ -1,42 +1,23 @@
-import { type valueof } from '../types'
-import { Uniform, type UniformType } from './Uniform'
+import { valueof } from '../types'
+import { Uniform, UniformType } from './Uniform'
 
-export type Uniforms = {
-    [key in keyof UniformType]?: {
-        [name: string]: UniformType[key]
-    }
-}
-
-type OptionalUniforms<UNIFORMS extends Uniforms> = {
-    [key in keyof Uniforms]?: {
-        [name in keyof UNIFORMS[key]]?: UniformType[key]
+type Uniforms = {
+    [key: string]: {
+        type: keyof UniformType
+        value: valueof<UniformType>
     }
 }
 
 export class UniformGroup<UNIFORMS extends Uniforms = Uniforms> {
     public uniforms: {
-        [key: string]: Uniform
+        [key in keyof UNIFORMS]: Uniform
     }
 
     constructor(uniforms: UNIFORMS) {
-        this.uniforms = {}
+        this.uniforms = {} as { [key in keyof UNIFORMS]: Uniform }
 
-        for (const [uniformType, _uniforms] of Object.entries(uniforms)) {
-            for (const [name, value] of Object.entries(_uniforms)) {
-                this.uniforms[name] = new Uniform(name, uniformType as keyof UniformType, value)
-            }
-        }
-    }
-
-    public setValues(uniforms: OptionalUniforms<UNIFORMS>) {
-        for (const _uniforms of Object.values(uniforms)) {
-            for (const [name, value] of Object.entries(_uniforms)) {
-                if (this.uniforms[name] && typeof this.uniforms[name].value !== 'undefined') {
-                    this.uniforms[name].value = value as valueof<UniformType>
-                } else {
-                    console.warn(`Uniform "${name}" is not found.`)
-                }
-            }
+        for (const [name, options] of Object.entries(uniforms)) {
+            this.uniforms[name as keyof UNIFORMS] = new Uniform(name, options.type, options.value)
         }
     }
 }
