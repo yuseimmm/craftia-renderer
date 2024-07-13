af43d9612897e37ce43412d144076e4c600493fc
yuseimmm <yusei.mmm@outlook.jp>
2024-07-13 11:46:49 +0900

    update:`RasterLayer` inherits from `Sprite`.

--- src/layer/RasterLayer.ts
+++ src/layer/RasterLayer.ts
@@ -1,49 +1,24 @@
-import { BLEND_MODES } from '../blend-modes'
-import { Container } from '../contanier'
-import { RenderStream } from '../stream'
-import { TextureSprite } from '../sprite'
-import { ILayer } from './ILayer'
-import { Vec2 } from '../units'
+import { Sprite, SpriteOptions } from '../sprite/Sprite'
 import { Texture } from '../textures'
 
-export type RasterLayerOptions = {
-    blendMode?: keyof typeof BLEND_MODES
-    translation?: Vec2
-    visible?: boolean
-    opacity?: number
-    fill?: number
-    texture: Texture
-}
+export type RasterLayerOptions = SpriteOptions
 
-export class RasterLayer extends Container implements ILayer<RasterLayerOptions> {
-    private _sprite: TextureSprite
-
-    constructor(options: RasterLayerOptions) {
-        super(options)
-
-        this._sprite = new TextureSprite({ texture: options.texture })
-    }
-
-    public get texture() {
-        return this._sprite.texture
-    }
-
+export class RasterLayer extends Sprite {
     public clone(options: Partial<RasterLayerOptions>) {
         return new RasterLayer({
             visible: this.visible,
             blendMode: this.blendMode,
             opacity: this.opacity,
             translation: this.translation,
-            fill: this.fill,
-            texture: this._sprite.texture,
+            texture: this.texture,
             ...options,
         })
     }
 
-    public renderFront(masterPipeline: RenderStream) {
-        this._sprite.setTransform(this.projectionMatrix)
-        this._sprite.render(masterPipeline.renderer, masterPipeline.front)
-
-        return masterPipeline.front.getColorTexture()
+    public get texture() {
+        return super.texture
+    }
+    public set texture(texture: Texture) {
+        super.texture = texture
     }
 }
