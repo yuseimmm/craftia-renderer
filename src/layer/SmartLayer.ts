33ee8ed0d4a05f57ce41e1f8a555c1bc8f63b023
yuseimmm <yusei.mmm@outlook.jp>
2024-07-18 22:16:44 +0900

    add SmartLayer

---
+++ src/layer/SmartLayer.ts
@@ -0,0 +1,62 @@
+import { mat3 } from 'gl-matrix'
+import { Sprite, SpriteOptions, SpriteUniformGroup } from '../sprite/Sprite'
+import { Texture } from '../textures'
+import { Vec2 } from '../units'
+import { Shader } from '../shader'
+
+export type SmartLayerOptions = SpriteOptions & {
+    rotation?: number
+    translation?: Vec2
+    scaling?: Vec2
+    transform?: mat3
+    shader?: Shader<SpriteUniformGroup>
+}
+
+export class SmartLayer extends Sprite {
+    constructor(options: SmartLayerOptions) {
+        super(options)
+
+        this.scaling = options.scaling ?? new Vec2(1, 1)
+        this.translation = options.translation ?? new Vec2(0, 0)
+        this.rotation = options.rotation ?? 0
+        this.transform = options.transform ?? mat3.create()
+    }
+
+    public clone(options: Partial<SmartLayerOptions>) {
+        return new SmartLayer({
+            ...this,
+            ...options,
+        })
+    }
+
+    public get texture() {
+        return super.texture
+    }
+    public set texture(texture: Texture) {
+        super.texture = texture
+    }
+
+    public get rotation() {
+        return super.rotation
+    }
+
+    public set rotation(rotation: number) {
+        super.rotation = rotation
+    }
+
+    public get transform() {
+        return super.transform
+    }
+
+    public set transform(transform: mat3) {
+        super.transform = transform
+    }
+
+    public get scaling() {
+        return super.scaling
+    }
+
+    public set scaling(scaling: Vec2) {
+        super.scaling = scaling
+    }
+}
--- src/layer/index.ts
+++ src/layer/index.ts
@@ -1,1 +1,2 @@
 export * from './RasterLayer'
+export * from './SmartLayer'
